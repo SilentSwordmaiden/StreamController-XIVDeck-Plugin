3bf2a22801c4fa74b41faaf5ee14010eb9d3b8b6
SilentSwordmaiden <swordmaiden@keemail.me>
2024-07-31 15:12:56 +0200

    Initial commit

---
+++ actions/SimpleAction/SimpleAction.py
@@ -0,0 +1,28 @@
+# Import StreamController modules
+from src.backend.PluginManager.ActionBase import ActionBase
+from src.backend.DeckManagement.DeckController import DeckController
+from src.backend.PageManagement.Page import Page
+from src.backend.PluginManager.PluginBase import PluginBase
+
+# Import python modules
+import os
+
+# Import gtk modules - used for the config rows
+import gi
+gi.require_version("Gtk", "4.0")
+gi.require_version("Adw", "1")
+from gi.repository import Gtk, Adw
+
+class SimpleAction(ActionBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def on_ready(self) -> None:
+        icon_path = os.path.join(self.plugin_base.PATH, "assets", "info.png")
+        self.set_media(media_path=icon_path, size=0.75)
+        
+    def on_key_down(self) -> None:
+        print("Key down")
+    
+    def on_key_up(self) -> None:
+        print("Key up")
---
+++ main.py
@@ -0,0 +1,27 @@
+# Import StreamController modules
+from src.backend.PluginManager.PluginBase import PluginBase
+from src.backend.PluginManager.ActionHolder import ActionHolder
+
+# Import actions
+from .actions.SimpleAction.SimpleAction import SimpleAction
+
+class PluginTemplate(PluginBase):
+    def __init__(self):
+        super().__init__()
+
+        ## Register actions
+        self.simple_action_holder = ActionHolder(
+            plugin_base = self,
+            action_base = SimpleAction,
+            action_id = "dev_core447_Template::SimpleAction", # Change this to your own plugin id
+            action_name = "Simple Action",
+        )
+        self.add_action_holder(self.simple_action_holder)
+
+        # Register plugin
+        self.register(
+            plugin_name = "Template",
+            github_repo = "https://github.com/StreamController/PluginTemplate",
+            plugin_version = "1.0.0",
+            app_version = "1.1.1-alpha"
+        )
